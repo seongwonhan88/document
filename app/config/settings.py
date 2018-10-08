ebda975a16694aec1a3fcdbf1970a855461441aa
Seongwon Han <seongwonhan88@gmail.com>
2018-10-08 16:50:16 +0900

    Pizza and Toppings added

--- app/config/settings.py
+++ app/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'fields',
     'foreignkey',
+    'many_to_many',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ app/many_to_many/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
+
+class FacebookUser(models.Model):
+    name = models.CharField(max_length=50)
