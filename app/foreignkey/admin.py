141d317c11969137ee3c920cb1f3b8520d942406
Seongwon Han <seongwonhan88@gmail.com>
2018-10-08 16:19:38 +0900

    fcuser class added

--- app/foreignkey/admin.py
+++ app/foreignkey/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Manufacturer, Car
+from .models import Manufacturer, Car, FCUser
 
 # Register your models here.
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+admin.site.register(FCUser)
--- app/foreignkey/models.py
+++ app/foreignkey/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        'Manufacturer', #문자열로 만들면 나중에 생성되더라도 오류나지 않음. 장고에서 지원
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
@@ -10,13 +20,16 @@ class Manufacturer(models.Model):
         return self.name
 
 
-class Car(models.Model):
-    manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE
-    )
+class FCUser(models.Model):
+    name = models.CharField(max_length=30)
+    instrucutor = models.ForeignKey(
+        'self',
+        blank = True,
+        null = True,
+        on_delete=models.SET_NULL, #해당 키가 삭제되면 자동으로 null처리
+        related_name='students',
 
-    name = models.CharField(max_length=50)
+    )
 
     def __str__(self):
         return self.name
