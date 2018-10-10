99b3bcebd18dbd1ee26b42e94e564fe25a00f645
Seongwon Han <seongwonhan88@gmail.com>
2018-10-10 10:00:04 +0900

    models added Person, Group, Membership

--- app/many_to_many/models.py
+++ app/many_to_many/models.py
@@ -19,13 +19,36 @@ class Pizza(models.Model):
 
 class FacebookUser(models.Model):
     name = models.CharField(max_length=50)
-    friend = models.ForeignKey(
-        'self',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='friend'
-    )
+
+    # friend = models.ForeignKey(
+    #     'self',
+    #     blank=True,
+    #     null=True,
+    #     on_delete=models.SET_NULL,
+    #     related_name='friend',
+    #  )
 
     def __str__(self):
         return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=80)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.CharField(max_length=80)
+    invite_reason = models.CharField(max_length=80)
