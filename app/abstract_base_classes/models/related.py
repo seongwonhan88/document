3965e68e0f3a9ee3ef993b9f010afb7a366cce42
Seongwon Han <seongwonhan88@gmail.com>
2018-10-11 16:37:59 +0900

    ABC related_name set bring error

---
+++ app/abstract_base_classes/models/related.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+__all__ = (
+    'Person',
+    'PostBase',
+    'PhotoPost',
+    'TextPost',
+)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class PostBase(models.Model):
+    author = models.ForeignKey(
+        'Person',
+        on_delete=models.CASCADE,
+        # 유저(Person) 입장에서
+        # 자신이 특정 post의 'author'인 경우
+        #
+        # 해당하는 모든 PostBase객체를 참조하는 역방향 매니저 이름
+        related_name='posts',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class PhotoPost(PostBase):
+    photo_url = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'Post(author: {self.author.name})'
+
+
+class TextPost(PostBase):
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Post(author: {self.author.name})'
